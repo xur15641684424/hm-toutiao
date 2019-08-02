8b59e12bf29887b1675f41985b506676755c2541
xuruihong <1483556570@qq.com>
2019-08-02 10:45:21 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+// render 渲染函数 h是回调的参数 渲染APP组件 挂载到App容器 把APP根组件渲染到APP容器
 new Vue({
   render: h => h(App)
 }).$mount('#app')
