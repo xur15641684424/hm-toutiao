bf3c7749cd334300de4714a896c81393c18c6d0c
xuruihong <1483556570@qq.com>
2019-08-03 15:07:18 +0800

    使用element-ui和vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import router from '@/router'
 Vue.config.productionTip = false
 // render 渲染函数 h是回调的参数 渲染APP组件 挂载到App容器 把APP根组件渲染到APP容器
+
+Vue.use(ElementUI)
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
